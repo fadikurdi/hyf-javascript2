a3bbfad6e61a375cf436f4252b29f9ebe0162793
Fadi <fadikurdi00@gmail.com>
2018-01-28 20:55:43 +0100

    Update mapAndFilter.js
    fixed time filtering on line 91

--- week2/mapAndFilter.js
+++ week2/mapAndFilter.js
@@ -14,12 +14,12 @@ let newNumbers;
 console.log("////////////-1-//////////////");
 
 let oddNumbers = numbers.filter(x => x % 2 !== 0);
-console.log("the odd numbers : "+oddNumbers);
+console.log("the odd numbers : " + oddNumbers);
 
 console.log("//////////////////////////");
 
 newNumbers = oddNumbers.map(x => x * 2);
-console.log("odd numbers doubled : "+newNumbers);
+console.log("odd numbers doubled : " + newNumbers);
 
 console.log("//////////////////////////");
 
@@ -72,14 +72,14 @@ let tasks = monday.concat(tuesday);
 
 console.log("////////////-2-//////////////");
 var totalDuration = 0;
-tasks.forEach((x) => x = totalDuration = totalDuration+ x.duration);
-console.log("total time is : " + totalDuration +" minutes");
+tasks.forEach((x) => x = totalDuration = totalDuration + x.duration);
+console.log("total time is : " + totalDuration + " minutes");
 console.log("//////////////////////////");
 
 
 console.log("////////////-3-//////////////");
 
-var totalHours = tasks.map(x => x.duration/60);
+var totalHours = tasks.map(x => x.duration / 60);
 
 console.log(totalHours);
 
@@ -88,14 +88,14 @@ console.log("//////////////////////////");
 
 console.log("////////////-4-//////////////");
 
-var newTimes = totalHours.filter(x => x >= 2);
+var newTimes = totalHours.filter(x => x < 2);
 console.log(newTimes);
 console.log("//////////////////////////");
 
 console.log("////////////-5-//////////////");
 
-var Maartje = newTimes.map(hour => hour * 15);
+var Maartje = newTimes.map(hour => hour * 30);
 console.log(Maartje);
 var sumUp = 0;
 Maartje.map((a) => a = sumUp = sumUp + a);
-console.log(sumUp +" €");
+console.log(sumUp + " €");
